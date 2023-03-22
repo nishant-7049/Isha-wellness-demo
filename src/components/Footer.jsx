1145f63722a9d5683d980531cb7df16f4fa0abfc
Himanshu Rathore <himanshu.rathore2561@gmail.com>
2023-03-22 22:14:46 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ import { AiFillYoutube } from 'react-icons/ai'
 import { AiFillLinkedin } from 'react-icons/ai'
 
 const Footer = () => {
-  const { ref, inView } = useInView({ threshold: 0.5 })
+  const { ref, inView } = useInView({ threshold: 0.2 })
   const animation = useAnimation()
 
   useEffect(() => {
@@ -16,10 +16,11 @@ const Footer = () => {
       animation.start({
         opacity: 1,
         y: 0,
-        transition: { duration: 1 },
+        transition: { duration: 0.5 },
       })
     }
   }, [inView])
+  
   return (
     <motion.div
       ref={ref}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,161 +1,183 @@
-import React from "react";
-import styled from "styled-components";
-import Achievement from "../components/About/Achievement";
+import React, { useEffect } from 'react'
+import { motion, useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+import styled from 'styled-components'
+import TechTeam from '../components/About/TechTeam'
+import TherTeam from '../components/About/TherTeam'
+import Achievement from '../components/About/Achievement'
 
 const About = () => {
+  const { ref, inView } = useInView({ threshold: 0 })
+  const animation = useAnimation()
+
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        opacity: 1,
+        y: 0,
+        transition: { type: 'spring', duration: 0.5, bounce: 0.5 },
+      })
+    }
+  }, [inView])
   return (
-    <Container>
-      <h1>
-        a<span>bou</span>t us
-      </h1>
-      <section className="about">
-        <div className="main">
-          <img
-            src="https://images.pexels.com/photos/5867730/pexels-photo-5867730.jpeg?auto=compress&cs=tinysrgb&w=600"
-            alt="/"
-          />
-          <div className="about-text">
-            <h5>
-              our<span> vision</span>{" "}
-            </h5>
-            <p>
-              Dr. Minjan Patel and Dr. Hitesh Purohit had a shared vision to
-              develop a system that would provide accessible and effective
-              treatment for patients in need of physiotherapy and occupational
-              therapy. The two met at the Indian Institute of Public Health
-              Gandhinagar and their idea was strengthened. With a shared passion
-              for public health and research, they established the organization
-              AKAS during their post-graduate studies. After working for 6 years
-              in Public Health Management and Research with various field NGOs
-              in urban, rural, and tribal areas, they realized that the problems
-              they sought to address were not limited to just urban areas and
-              were causing even more suffering in rural and tribal areas. With
-              their combined expertise in their respective therapies, extensive
-              knowledge of public health and research, and a dedication to solve
-              this problem , they developed treatment guidelines that were
-              highly effective, more active than passive, easy to execute, and
-              didn't relay on electrotherapy machines for recovery and
-              sustainability. The next step was to train individuals with
-              minimal background to execute the treatments under their guidance.
-              As their experience grew, they realized that their approach could
-              be scalable, replicable, and self-sustainable if supported by
-              technology for supportive supervision and monitoring purposes.
-              With their successful treatment model and the realization that
-              expansion of services and research would require significant
-              funding support, Dr. Minjan Patel and Dr. Hitesh Purohit made the
-              decision to register a private limited company called 'Health
-              Prism Limited'(HPL). The purpose of Health Prism was to develop
-              and manage centres in urban areas to generate enough funds for
-              research and provide services in rural and tribal areas where
-              demand and paying capacity were not sufficient.Health Prism was
-              established with the goal of improving access to effective
-              physiotherapy and occupational therapy for all, regardless of
-              location or financial background. With a combination of expertise,
-              dedication, and innovative thinking, Dr. Patel and Dr. Purohit
-              have been able to bring their vision to life and provide
-              life-changing treatment to those in need.
-            </p>
-          </div>
-        </div>
-      </section>
-      <div className="founder ">
-        Founder of<span> Ishawellness center</span>
-      </div>
-      <section className="hum">
-        <div className="tum">
-          <div className="job">
+    <motion.div ref={ref} initial={{ opacity: 0, y: 100 }} animate={animation}>
+      <Container>
+        <h1>
+          a<span>bou</span>t us
+        </h1>
+        <section className='about'>
+          <div className='main'>
             <img
-              src="https://images.pexels.com/photos/5867730/pexels-photo-5867730.jpeg?auto=compress&cs=tinysrgb&w=600"
-              alt="/"
+              src='https://images.pexels.com/photos/5867730/pexels-photo-5867730.jpeg?auto=compress&cs=tinysrgb&w=600'
+              alt='/'
             />
+            <div className='about-text'>
+              <h5>
+                our<span> vision</span>{' '}
+              </h5>
+              <p>
+                Dr. Minjan Patel and Dr. Hitesh Purohit had a shared vision to
+                develop a system that would provide accessible and effective
+                treatment for patients in need of physiotherapy and occupational
+                therapy. The two met at the Indian Institute of Public Health
+                Gandhinagar and their idea was strengthened. With a shared
+                passion for public health and research, they established the
+                organization AKAS during their post-graduate studies. After
+                working for 6 years in Public Health Management and Research
+                with various field NGOs in urban, rural, and tribal areas, they
+                realized that the problems they sought to address were not
+                limited to just urban areas and were causing even more suffering
+                in rural and tribal areas. With their combined expertise in
+                their respective therapies, extensive knowledge of public health
+                and research, and a dedication to solve this problem , they
+                developed treatment guidelines that were highly effective, more
+                active than passive, easy to execute, and didn't relay on
+                electrotherapy machines for recovery and sustainability. The
+                next step was to train individuals with minimal background to
+                execute the treatments under their guidance. As their experience
+                grew, they realized that their approach could be scalable,
+                replicable, and self-sustainable if supported by technology for
+                supportive supervision and monitoring purposes. With their
+                successful treatment model and the realization that expansion of
+                services and research would require significant funding support,
+                Dr. Minjan Patel and Dr. Hitesh Purohit made the decision to
+                register a private limited company called 'Health Prism
+                Limited'(HPL). The purpose of Health Prism was to develop and
+                manage centres in urban areas to generate enough funds for
+                research and provide services in rural and tribal areas where
+                demand and paying capacity were not sufficient.Health Prism was
+                established with the goal of improving access to effective
+                physiotherapy and occupational therapy for all, regardless of
+                location or financial background. With a combination of
+                expertise, dedication, and innovative thinking, Dr. Patel and
+                Dr. Purohit have been able to bring their vision to life and
+                provide life-changing treatment to those in need.
+              </p>
+            </div>
           </div>
-          <div className="kon">
-            <h5>
-              Dr.<span>hitesh purohit</span>{" "}
-            </h5>
-            <p>
-              Dr. Hitesh Purohit is the Founder Director and President of AKAS
-              and ISHA Wellness Centre. He holds a Bachelor's degree in Physical
-              Therapy and a Post-Graduation in Public Health. Dr. Purohit is a
-              highly skilled and passionate entrepreneur leader with a deep
-              commitment to tech-led innovative ideas in organizing need-based
-              systems for community health. With a wealth of experience and
-              expertise in physical therapy and public health, Dr. Purohit is
-              well-equipped to drive positive change and make a real difference
-              to the lives of people in the community.
-            </p>
-          </div>
+        </section>
+        <Achievement />
+        <div className='founder '>
+          Founder of<span> Ishawellness center</span>
         </div>
-      </section>
-      <section className="hum">
-        <div className="tum">
-          <div className="job">
-            <img
-              src="https://images.pexels.com/photos/5867730/pexels-photo-5867730.jpeg?auto=compress&cs=tinysrgb&w=600"
-              alt="/"
-            />
+        <section className='hum'>
+          <div className='tum'>
+            <div className='job'>
+              <img
+                src='https://images.pexels.com/photos/5867730/pexels-photo-5867730.jpeg?auto=compress&cs=tinysrgb&w=600'
+                alt='/'
+              />
+            </div>
+            <div className='kon'>
+              <h5>
+                Dr.<span>hitesh purohit</span>{' '}
+              </h5>
+              <p>
+                Dr. Hitesh Purohit is the Founder Director and President of AKAS
+                and ISHA Wellness Centre. He holds a Bachelor's degree in
+                Physical Therapy and a Post-Graduation in Public Health. Dr.
+                Purohit is a highly skilled and passionate entrepreneur leader
+                with a deep commitment to tech-led innovative ideas in
+                organizing need-based systems for community health. With a
+                wealth of experience and expertise in physical therapy and
+                public health, Dr. Purohit is well-equipped to drive positive
+                change and make a real difference to the lives of people in the
+                community.
+              </p>
+            </div>
           </div>
-          <div className="kon">
-            <h5>
-              Dr.<span>minjan patel</span>{" "}
-            </h5>
-            <p>
-              Dr. Minjan Patel is the Founder Director and CEO at AKAS and Isha
-              Wellness Centre. With a Bachelor's degree in Occupational Therapy
-              and a post-graduation in Public Health, she has further honed her
-              skills with a Master's in Rural Development. She is passionate
-              about finding research-based solutions to improve community health
-              through empowering and uplifting s communities through their
-              participation and service provision. Dr. Patel is dedicated to
-              making a positive impact on the health and well- being of
-              communities.
-            </p>
+        </section>
+        <section className='hum'>
+          <div className='tum'>
+            <div className='job'>
+              <img
+                src='https://images.pexels.com/photos/5867730/pexels-photo-5867730.jpeg?auto=compress&cs=tinysrgb&w=600'
+                alt='/'
+              />
+            </div>
+            <div className='kon'>
+              <h5>
+                Dr.<span>minjan patel</span>{' '}
+              </h5>
+              <p>
+                Dr. Minjan Patel is the Founder Director and CEO at AKAS and
+                Isha Wellness Centre. With a Bachelor's degree in Occupational
+                Therapy and a post-graduation in Public Health, she has further
+                honed her skills with a Master's in Rural Development. She is
+                passionate about finding research-based solutions to improve
+                community health through empowering and uplifting s communities
+                through their participation and service provision. Dr. Patel is
+                dedicated to making a positive impact on the health and well-
+                being of communities.
+              </p>
+            </div>
           </div>
+        </section>
+
+        <div className='daada'>
+          About <span>ishawellness center</span>
         </div>
-      </section>
-      <div className="daada">
-        About <span>ishawellness center</span>
-      </div>
-      <div className="papa">
-        <h3>
-          Isha Wellness Centre is a leading provider of holistic wellness
-          services. The centre offers a wide range of services aimed at
-          promoting physical, mental, and emotional well-being. With a team of
-          highly trained and experienced wellness facilitators, Isha Wellness
-          Centre is dedicated to helping people lead healthier and happier lives
-          At Isha Wellness Centre, a personalized approach is taken to meet the
-          unique needs of each individual. Whether it's through yoga,
-          meditation, nutrition, or any other aspect of wellness, the centre's
-          goal is to empower people to take control of their health and
-          happiness. With a focus on both prevention and healing, the services
-          offered at Isha Wellness Centre are designed to support individuals in
-          all stages of their wellness journey.In addition to one-on-one
-          consultations, Isha Wellness Centre also offers group classNamees,
-          workshops, and retreats. These programs provide opportunities for
-          individuals to learn, grow, and connect with others who share a
-          similar commitment to wellness. With a commitment to education and
-          research, Isha Wellness Centre stays at the forefront of the latest
-          developments in the field of holistic wellness.In addition to
-          one-on-one consultations, Isha Wellness Centre also offers group
-          classNamees, workshops, and retreats. These programs provide
-          opportunities for individuals to learn, grow, and connect with others
-          who share a similar commitment to wellness. With a commitment to
-          education and research, Isha Wellness Centre stays at the forefront of
-          the latest developments in the field of holistic wellness. Overall,
-          Isha Wellness Centre is a dynamic and thriving hub for wellness that
-          offers a comprehensive and holistic approach to health and happiness.
-          Whether you're looking to improve your physical health, manage stress,
-          or simply cultivate a deeper sense of inner peace, Isha Wellness
-          Centre is the perfect place to start.
-        </h3>
-      </div>
+        <div className='papa'>
+          <h3>
+            Isha Wellness Centre is a leading provider of holistic wellness
+            services. The centre offers a wide range of services aimed at
+            promoting physical, mental, and emotional well-being. With a team of
+            highly trained and experienced wellness facilitators, Isha Wellness
+            Centre is dedicated to helping people lead healthier and happier
+            lives At Isha Wellness Centre, a personalized approach is taken to
+            meet the unique needs of each individual. Whether it's through yoga,
+            meditation, nutrition, or any other aspect of wellness, the centre's
+            goal is to empower people to take control of their health and
+            happiness. With a focus on both prevention and healing, the services
+            offered at Isha Wellness Centre are designed to support individuals
+            in all stages of their wellness journey.In addition to one-on-one
+            consultations, Isha Wellness Centre also offers group classNamees,
+            workshops, and retreats. These programs provide opportunities for
+            individuals to learn, grow, and connect with others who share a
+            similar commitment to wellness. With a commitment to education and
+            research, Isha Wellness Centre stays at the forefront of the latest
+            developments in the field of holistic wellness.In addition to
+            one-on-one consultations, Isha Wellness Centre also offers group
+            classNamees, workshops, and retreats. These programs provide
+            opportunities for individuals to learn, grow, and connect with
+            others who share a similar commitment to wellness. With a commitment
+            to education and research, Isha Wellness Centre stays at the
+            forefront of the latest developments in the field of holistic
+            wellness. Overall, Isha Wellness Centre is a dynamic and thriving
+            hub for wellness that offers a comprehensive and holistic approach
+            to health and happiness. Whether you're looking to improve your
+            physical health, manage stress, or simply cultivate a deeper sense
+            of inner peace, Isha Wellness Centre is the perfect place to start.
+          </h3>
+        </div>
+        <TechTeam />
+        <TherTeam />
+      </Container>
+    </motion.div>
+  )
+}
 
-      <Achievement />
-    </Container>
-  );
-};
-
-export default About;
+export default About
 
 const Container = styled.div`
   @import url("https://fonts.googleapis.com/css2?family=Poppins&family=Work+Sans&display=swap");
@@ -173,7 +195,7 @@ const Container = styled.div`
   .about {
     width: 100%;
     padding: 2rem;
-    background-color: rgb(153, 203, 236);
+    background-color: #50acfb;
     border-radius: 1rem;
   }
   .about img {
@@ -194,7 +216,7 @@ const Container = styled.div`
   }
   .about-text h5 {
     font-size: x-large;
-    border-bottom: 3px solid green;
+    border-bottom: 3px solid blue;
     width: 20%;
     margin-bottom: 1rem;
   }
@@ -212,11 +234,13 @@ const Container = styled.div`
     margin-bottom: 2rem;
   }
   .hum {
-    background-color: rgb(153, 203, 236);
+    background-color:white;
     padding: 3rem;
     margin-top: 3rem;
-    border-radius: 1rem;
-    box-shadow: inset 3px 300px blue;
+    border-radius: .5rem;
+    box-shadow: inset 0px 300px #50acfb;
+    border: 1px solid #50acfb;
+    
   }
   .job {
     width: fit-content;
@@ -227,7 +251,7 @@ const Container = styled.div`
     border-radius: 80%;
     height: 350px;
     width: 350px;
-    box-shadow: 10px 13px green;
+    box-shadow: 5px 10px 15px 8px rgba(0, 0, 0, 0.5);
   }
   .kon h5 {
     margin: 1rem auto;
@@ -237,7 +261,7 @@ const Container = styled.div`
     font-size: x-large;
     border-bottom: 3px solid black;
 
-    border-color: green;
+    border-color: blue;
   }
 
   .kon p {
@@ -262,8 +286,8 @@ const Container = styled.div`
     margin-bottom: 5rem;
   }
 
-  h3 {
-    background-color: rgb(172, 210, 245);
+  .papa h3 {
+    background-color: #50acfb;
     color: black;
     padding: 2rem;
     border-radius: 1rem;
@@ -290,4 +314,4 @@ const Container = styled.div`
     width: 30%;
     border-color: green;
   }
-`;
+`
