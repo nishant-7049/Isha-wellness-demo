13b5e468a45a858c0d31286a348f5c2c1b4553ef
nishant-7049 <nishantsharmay2@gmail.com>
2023-08-02 16:19:18 +0530

    origin changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
+import http from "https";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://ishacare.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
