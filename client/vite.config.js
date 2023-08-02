068eb3607a98686b17b049bb168fce685937b352
nishant-7049 <nishantsharmay2@gmail.com>
2023-08-02 16:00:11 +0530

    big changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:6000",
+        target: "https://ishacare.onrender.com",
         changeOrigin: true,
         secure: false,
       },
