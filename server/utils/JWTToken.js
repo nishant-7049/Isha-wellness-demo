4d93759be1e46c4f069b3878b2a090295d4cf0c7
nishant-7049 <nishantsharmay2@gmail.com>
2023-08-08 14:44:06 +0530

    sameSite none

--- server/utils/JWTToken.js
+++ server/utils/JWTToken.js
@@ -5,7 +5,7 @@ const setToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    sameSite: "strict",
+    sameSite: "None",
     secure: true,
     httpOnly: true,
   };
