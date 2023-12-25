6f051eba14c27673783655c5bf519e4836520011
nishant-7049 <nishantsharmay2@gmail.com>
2023-12-25 19:18:55 +0530

    added Admin

---
+++ client/src/admin/components/Dashboard.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import Sidebar from "./Sidebar";
+import AdminPatientMeter from "./AdminPatientMeter";
+import CenterReport from "./CenterReport";
+import { useDispatch } from "react-redux";
+import {
+  getCentreData,
+  getPatientMeterData,
+} from "../../store/slices/dashboardSlice";
+
+const Dashboard = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPatientMeterData());
+    dispatch(getCentreData());
+  }, []);
+  return (
+    <div className="flex sm:flex-col">
+      <Sidebar />
+      <div className="w-full">
+        <div className="bg-[#F0F0F0]">
+          <div className=" bg-white my-[6vmax] ">
+            <h1 className="text-[#00286b] text-3xl font-bold border-b-4 border-[#00286b] pb-2 w-fit mx-auto">
+              Dashboard
+            </h1>
+            <AdminPatientMeter />
+            <CenterReport />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
