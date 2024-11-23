62a08ea71ffd309e3805cbbac8d1e8b803904aab
nishant-7049 <nishantsharmay2@gmail.com>
2024-11-23 17:54:46 +0530

    making packages with proper payment method and not leaving it empty

--- server/utils/JWTToken.js
+++ server/utils/JWTToken.js
@@ -2,13 +2,13 @@ const setToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
 
   const options = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    path: "/",
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    path: "/",
-    sameSite: "None",
-    secure: true,
-    httpOnly: true,
   };
   res.cookie("token", token, options);
   res.status(statusCode).json({
